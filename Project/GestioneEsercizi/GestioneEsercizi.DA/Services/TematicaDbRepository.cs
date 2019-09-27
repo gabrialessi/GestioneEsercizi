fcf5b83d8dd1bd3cb930f92a50825f376637fca8
gabrialessi <gabrialessi23@gmail.com>
2019-09-27 16:06:33 +0200

    Create TematicaDbRepository.cs

---
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Services/TematicaDbRepository.cs
@@ -0,0 +1,15 @@
+﻿using GestioneEsercizi.DA.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestioneEsercizi.DA.Services
+{
+    public class TematicaDbRepository : DbDataRepository<AppDbContext, Tematica>, ITematicaRepository
+    {
+        public TematicaDbRepository(AppDbContext ctx) : base(ctx)
+        {
+
+        }
+    }
+}
