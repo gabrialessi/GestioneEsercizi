5d739c3db5302ef0b753a1e75f26a1c0102fb11b
gabrialessi <gabrialessi23@gmail.com>
2019-09-27 16:06:30 +0200

    Create ClasseDbRepository.cs

---
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Services/ClasseDbRepository.cs
@@ -0,0 +1,15 @@
+﻿using GestioneEsercizi.DA.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestioneEsercizi.DA.Services
+{
+    public class ClasseDbRepository : DbDataRepository<AppDbContext, Classe>, IClasseRepository
+    {
+        public ClasseDbRepository(AppDbContext ctx) : base(ctx)
+        {
+
+        }
+    }
+}
