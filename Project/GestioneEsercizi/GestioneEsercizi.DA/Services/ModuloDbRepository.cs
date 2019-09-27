6607476a27f6591534e9dac0ee135660b7cd0a6b
gabrialessi <gabrialessi23@gmail.com>
2019-09-27 16:03:14 +0200

    Create ModuloDbRepository.cs

---
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Services/ModuloDbRepository.cs
@@ -0,0 +1,15 @@
+﻿using GestioneEsercizi.DA.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestioneEsercizi.DA.Services
+{
+    public class ModuloDbRepository : DbDataRepository<AppDbContext, Modulo>, IModuloRepository
+    {
+        public ModuloDbRepository(AppDbContext ctx) : base(ctx)
+        {
+
+        }
+    }
+}
