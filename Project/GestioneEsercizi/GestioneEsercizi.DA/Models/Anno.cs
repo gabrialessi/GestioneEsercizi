56e1ade2f91ad0ef4a3187f8ffb16721875e8945
gabrialessi <gabrialessi23@gmail.com>
2019-10-01 13:49:52 +0200

    creato model Anno

---
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Models/Anno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestioneEsercizi.DA.Models
+{
+    public class Anno : BaseEntity
+    {
+        public string Annata { get; set; }
+        public ICollection<Classe> Classi { get; set; }
+        public ICollection<Modulo> Moduli { get; set; }
+        public ICollection<Prova> Prove { get; set; }
+    }
+}
