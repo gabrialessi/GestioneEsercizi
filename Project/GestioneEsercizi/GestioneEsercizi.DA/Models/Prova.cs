3fbfff1bcd7dcc22ccbf7a971586d1428f48e6f0
gabrialessi <gabrialessi23@gmail.com>
2019-10-01 13:49:31 +0200

    creato model Prova

---
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Models/Prova.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestioneEsercizi.DA.Models
+{
+    public class Prova : BaseEntity
+    {
+        public string Titolo { get; set; }
+        public DateTime Data { get; set; }
+        public ICollection<EsercizioProva> EserciziProva { get; set; }
+    }
+}
