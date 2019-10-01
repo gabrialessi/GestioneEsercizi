3ff7c860c5f77d3695a024370f93bdacf1d1d51a
gabrialessi <gabrialessi23@gmail.com>
2019-10-01 13:49:08 +0200

    creato model Esercizio

---
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Models/Esercizio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestioneEsercizi.DA.Models
+{
+    public class Esercizio : BaseEntity
+    {
+        public string Titolo { get; set; }
+        public string Testo { get; set; }
+    }
+}
