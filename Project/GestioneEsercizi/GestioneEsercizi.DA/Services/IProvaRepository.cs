aa727727cd86cd2bb81eff9d3bd37e7a6b56531c
gabrialessi <gabrialessi23@gmail.com>
2019-10-04 14:35:23 +0200

    IProvaRepository concluso

---
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Services/IProvaRepository.cs
@@ -0,0 +1,15 @@
+﻿using GestioneEsercizi.DA.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestioneEsercizi.DA.Services
+{
+    /// <summary>
+    /// Interfaccia che implementa i metodi relativi al database
+    /// sul modello di dati della prova.
+    /// </summary>
+    public interface IProvaRepository : IDataRepository<Prova>
+    {
+    }
+}
