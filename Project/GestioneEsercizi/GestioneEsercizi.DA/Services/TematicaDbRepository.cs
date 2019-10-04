a196f62b7e2265d47527ed3ff7aeb6d5e712a8d8
gabrialessi <gabrialessi23@gmail.com>
2019-10-04 14:06:37 +0200

    TematicaDbRepository concluso

--- Project/GestioneEsercizi/GestioneEsercizi.DA/Services/TematicaDbRepository.cs
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Services/TematicaDbRepository.cs
@@ -1,15 +1,31 @@
 ﻿using GestioneEsercizi.DA.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GestioneEsercizi.DA.Services
 {
+    /// <summary>
+    /// Deposito dati relativo al modello di dati della tematica.
+    /// </summary>
     public class TematicaDbRepository : DbDataRepository<AppDbContext, Tematica>, ITematicaRepository
     {
+        /// <summary>
+        /// Metodo costruttore, si definisce il contesto di dati.
+        /// </summary>
+        /// <param name="ctx">Contesto di dati del database.</param>
         public TematicaDbRepository(AppDbContext ctx) : base(ctx)
         {
+        }
 
+        /// <summary>
+        /// Ritorna tutte le entità ordinate per nome.
+        /// </summary>
+        /// <returns>Le entità ordinate per nome.</returns>
+        public override IQueryable<Tematica> Get()
+        {
+            return base.Get().OrderBy(s => s.Nome);
         }
     }
 }
