a78a82648e8919f898ad7812d0e793b01f7604ef
gabrialessi <gabrialessi23@gmail.com>
2019-10-04 14:27:46 +0200

    EsercizioDbRepository concluso

---
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Services/EsercizioDbRepository.cs
@@ -0,0 +1,31 @@
+﻿using GestioneEsercizi.DA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GestioneEsercizi.DA.Services
+{
+    /// <summary>
+    /// Deposito dati relativo al modello di dati dell'esercizio.
+    /// </summary>
+    public class EsercizioDbRepository : DbDataRepository<AppDbContext, Esercizio>, IEsercizioRepository
+    {
+        /// <summary>
+        /// Metodo costruttore, si definisce il contesto di dati.
+        /// </summary>
+        /// <param name="ctx">Contesto di dati del database.</param>
+        public EsercizioDbRepository(AppDbContext ctx) : base(ctx)
+        {
+        }
+
+        /// <summary>
+        /// Ritorna tutte le entità ordinate per titolo.
+        /// </summary>
+        /// <returns>Le entità ordinate per titolo.</returns>
+        public override IQueryable<Esercizio> Get()
+        {
+            return base.Get().OrderBy(s => s.Titolo);
+        }
+    }
+}
