35fcdc3a91535818a9e058469dde0a19ed3a575c
gabrialessi <gabrialessi23@gmail.com>
2019-10-04 14:31:39 +0200

    IEsercizioProvaRepositort concluso

---
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Services/IEsercizioProvaRepository.cs
@@ -0,0 +1,15 @@
+﻿using GestioneEsercizi.DA.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestioneEsercizi.DA.Services
+{
+    /// <summary>
+    /// Interfaccia che implementa i metodi relativi al database
+    /// sul modello di dati dell'esercizio della prova.
+    /// </summary>
+    public interface IEsercizioProvaRepository : IDataRepository<EsercizioProva>
+    {
+    }
+}
