77e2627c4bd30494930e78398c9da7167d60cee4
gabrialessi <gabrialessi23@gmail.com>
2019-10-04 13:51:31 +0200

    IModuloRepository concluso

--- Project/GestioneEsercizi/GestioneEsercizi.DA/Services/IModuloRepository.cs
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Services/IModuloRepository.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace GestioneEsercizi.DA.Services
 {
+    /// <summary>
+    /// Interfaccia che implementa i metodi relativi al database
+    /// sul modello di dati del modulo.
+    /// </summary>
     public interface IModuloRepository : IDataRepository<Modulo>
     {
     }
