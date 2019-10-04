754479b9230a80e488c41c1babea13c16d50dfa3
gabrialessi <gabrialessi23@gmail.com>
2019-10-04 15:31:42 +0200

    AppDbContext aggiornato

--- Project/GestioneEsercizi/GestioneEsercizi.DA/AppDbContext.cs
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/AppDbContext.cs
@@ -9,29 +9,71 @@ using System.Threading.Tasks;
 
 namespace GestioneEsercizi.DA
 {
+    /// <summary>
+    /// Classe che imposta le entità del database e configura
+    /// il percorso di memorizzazione della base di dati.
+    /// </summary>
     public class AppDbContext : DbContext
     {
+        /// <summary>
+        /// Raccolta delle entità delle classi.
+        /// </summary>
         public DbSet<Classe> Classi { get; set; }
 
+        /// <summary>
+        /// Raccolta delle entità dei moduli.
+        /// </summary>
         public DbSet<Modulo> Moduli { get; set; }
 
+        /// <summary>
+        /// Raccolta delle entità delle tematiche.
+        /// </summary>
         public DbSet<Tematica> Tematiche { get; set; }
 
+        /// <summary>
+        /// Raccolta delle entità degli anni.
+        /// </summary>
+        public DbSet<Anno> Anno { get; set; }
+
+        /// <summary>
+        /// Raccolta delle entità degli esercizi.
+        /// </summary>
+        public DbSet<Esercizio> Esercizi { get; set; }
+
+        /// <summary>
+        /// Raccolta delle entità degli esercizi delle prove.
+        /// </summary>
+        public DbSet<EsercizioProva> EserciziProva { get; set; }
+
+        /// <summary>
+        /// Raccolta delle entità delle prove.
+        /// </summary>
+        public DbSet<Prova> Prove { get; set; }
+
+        /// <summary>
+        /// Metodo costruttore vuoto (ereditato).
+        /// </summary>
         public AppDbContext() : base()
         {
-
         }
 
+        /// <summary>
+        /// Metodo costruttore utilizzato per preparare il database.
+        /// </summary>
+        /// <param name="options">Configurazione del database.</param>
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-
         }
 
+        /// <summary>
+        /// Configurazione del percorso di memorizzazione del database SQLite.
+        /// </summary>
+        /// <param name="optionsBuilder">Opzioni del contesto di dati presenti nel sistema.</param>
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlite("Data Source=D:\\Temp\\GestioneEsercizi.db");
+                optionsBuilder.UseSqlite("Data Source=D:\\GestioneEsercizi\\GestioneEsercizi.db");
             }
         }
     }
