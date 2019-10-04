6fa461fa8cd448c4b0245622b18c7499e67a9e51
gabrialessi <gabrialessi23@gmail.com>
2019-10-04 13:59:59 +0200

    ITematicaRepository concluso

--- Project/GestioneEsercizi/GestioneEsercizi.DA/Services/ITematicaRepository.cs
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Services/ITematicaRepository.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace GestioneEsercizi.DA.Services
 {
+    /// <summary>
+    /// Interfaccia che implementa i metodi relativi al database
+    /// sul modello di dati della tematica.
+    /// </summary>
     public interface ITematicaRepository : IDataRepository<Tematica>
     {
     }
