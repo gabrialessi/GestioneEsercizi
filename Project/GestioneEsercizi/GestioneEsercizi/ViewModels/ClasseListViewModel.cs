264faea65610a06b6d0fae429a6d031128bfc6dd
gabrialessi <gabrialessi23@gmail.com>
2019-10-08 16:16:13 +0200

    ClasseListViewModel concluso

---
+++ Project/GestioneEsercizi/GestioneEsercizi/ViewModels/ClasseListViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GestioneEsercizi.DA;
+using GestioneEsercizi.DA.Models;
+using GestioneEsercizi.DA.Services;
+using MVVM;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace GestioneEsercizi.ViewModels
+{
+    public class ClasseListViewModel : BindableBase
+    {
+        /// <summary>
+        /// Insieme dinamico dei dati delle classi.
+        /// </summary>
+        public ObservableCollection<Classe> Classi { get; set; }
+
+        /// <summary>
+        /// Metodo costruttore del ViewModel.
+        /// </summary>
+        public ClasseListViewModel()
+        {
+            ClasseDbRepository repo = new ClasseDbRepository(new AppDbContext());
+            Classi = new ObservableCollection<Classe>(repo.Get());
+        }
+    }
+}
