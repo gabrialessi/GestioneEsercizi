3c9cfc6c1c7b4034de983566591f6ca804555774
gabrialessi <gabrialessi23@gmail.com>
2019-10-10 15:22:13 +0200

    EsercizioViewModel concluso

---
+++ Project/GestioneEsercizi/GestioneEsercizi/ViewModels/EsercizioViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GestioneEsercizi.DA;
+using GestioneEsercizi.DA.Models;
+using GestioneEsercizi.DA.Services;
+using MVVM;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace GestioneEsercizi.ViewModels
+{
+    public class EsercizioViewModel : BindableBase
+    {
+        /// <summary>
+        /// Insieme dinamico dei dati degli esercizi.
+        /// </summary>
+        public ObservableCollection<Esercizio> Esercizi { get; set; }
+
+        /// <summary>
+        /// Metodo costruttore del ViewModel.
+        /// </summary>
+        public EsercizioViewModel()
+        {
+            EsercizioDbRepository repo = new EsercizioDbRepository(new AppDbContext());
+            Esercizi = new ObservableCollection<Esercizio>(repo.Get());
+        }
+    }
+}
