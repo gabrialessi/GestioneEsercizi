0c691a07f579f8e7da55b59ee4f6485d76c10665
gabrialessi <gabrialessi23@gmail.com>
2019-10-10 14:15:12 +0200

    TematicaListViewModel concluso

---
+++ Project/GestioneEsercizi/GestioneEsercizi/ViewModels/TematicaListViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GestioneEsercizi.DA;
+using GestioneEsercizi.DA.Models;
+using GestioneEsercizi.DA.Services;
+using MVVM;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace GestioneEsercizi.ViewModels
+{
+    public class TematicaListViewModel : BindableBase
+    {
+        /// <summary>
+        /// Insieme dinamico dei dati delle tematiche.
+        /// </summary>
+        public ObservableCollection<Tematica> Tematiche { get; set; }
+
+        /// <summary>
+        /// Metodo costruttore del ViewModel.
+        /// </summary>
+        public TematicaListViewModel()
+        {
+            TematicaDbRepository repo = new TematicaDbRepository(new AppDbContext());
+            Tematiche = new ObservableCollection<Tematica>(repo.Get());
+        }
+    }
+}
