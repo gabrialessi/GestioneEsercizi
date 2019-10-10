b727db9243ad034042498c81c90ff753b51c19c3
gabrialessi <gabrialessi23@gmail.com>
2019-10-10 14:08:12 +0200

    ModuloListViewModel concluso

---
+++ Project/GestioneEsercizi/GestioneEsercizi/ViewModels/ModuloListViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GestioneEsercizi.DA;
+using GestioneEsercizi.DA.Models;
+using GestioneEsercizi.DA.Services;
+using MVVM;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace GestioneEsercizi.ViewModels
+{
+    public class ModuloListViewModel : BindableBase
+    {
+        /// <summary>
+        /// Insieme dinamico dei dati dei moduli.
+        /// </summary>
+        public ObservableCollection<Modulo> Moduli { get; set; }
+
+        /// <summary>
+        /// Metodo costruttore del ViewModel.
+        /// </summary>
+        public ModuloListViewModel()
+        {
+            ModuloDbRepository repo = new ModuloDbRepository(new AppDbContext());
+            Moduli = new ObservableCollection<Modulo>(repo.Get());
+        }
+    }
+}
