094c1bda5361983a7ee8fd2aa3d429b5dc7b8a45
gabrialessi <gabrialessi23@gmail.com>
2019-10-24 16:14:05 +0200

    EsercizioListViewModel creato

---
+++ Project/GestioneEsercizi/GestioneEsercizi/ViewModels/EsercizioListViewModel.cs
@@ -0,0 +1,46 @@
+﻿using GestioneEsercizi.DA;
+using GestioneEsercizi.DA.Models;
+using GestioneEsercizi.DA.Services;
+using MVVM;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace GestioneEsercizi.ViewModels
+{
+    /// <summary>
+    /// ViewModel della schermata della lista di esercizi memorizzati.
+    /// </summary>
+    public class EsercizioListViewModel : BindableBase
+    {
+        private BenvenutoViewModel benvenutoViewModel;
+        public IDelegateCommand BenvenutoCommand { get; set; }
+        /// <summary>
+        /// Lista di esercizi.
+        /// </summary>
+        public ObservableCollection<Esercizio> Esercizi { get; set; }
+
+        /// <summary>
+        /// Metodo costruttore del ViewModel.
+        /// </summary>
+        public EsercizioListViewModel()
+        {
+            RegisterCommands();
+            EsercizioDbRepository repo = new EsercizioDbRepository(new AppDbContext());
+            Esercizi = new ObservableCollection<Esercizio>(repo.Get());
+        }
+
+        /// <summary>
+        /// Impostazione dei ViewModels e Commands relativi.
+        /// </summary>
+        private void RegisterCommands()
+        {
+            benvenutoViewModel = new BenvenutoViewModel();
+            BenvenutoCommand = new DelegateCommand(OnBenvenuto, CanBenvenuto);
+        }
+
+        private void OnBenvenuto(object obj) { Messenger.Default.Send<BindableBase>(benvenutoViewModel); }
+        private bool CanBenvenuto(object arg) { return true; }
+    }
+}
--- Project/GestioneEsercizi/GestioneEsercizi/ViewModels/MainViewModel.cs
+++ Project/GestioneEsercizi/GestioneEsercizi/ViewModels/MainViewModel.cs
@@ -5,45 +5,69 @@ using System.Text;
 
 namespace GestioneEsercizi.ViewModels
 {
+    /// <summary>
+    /// ViewModel principale in cui vengono gestiti gli altri ViewModels
+    /// e viene impostato quello da usare tramite i Commands e la 
+    /// proprietà CurrentViewModel.
+    /// </summary>
     public class MainViewModel : BindableBase
     {
         private BenvenutoViewModel benvenutoViewModel;
         private AboutViewModel aboutViewModel;
-
+        private EsercizioListViewModel esercizioListViewModel;
         public IDelegateCommand BenvenutoCommand { get; set; }
         public IDelegateCommand AboutCommand { get; set; }
+        public IDelegateCommand EsercizioListCommand { get; set; }
 
         private BindableBase currentViewModel;
+        /// <summary>
+        /// Proprietà che rappresenta il ViewModel da mostrare nella MainView.
+        /// </summary>
         public BindableBase CurrentViewModel
         {
             get { return currentViewModel; }
             set { SetProperty(ref currentViewModel, value); }
         }
 
-        public MainViewModel()
-        {
-            RegisterCommands();
-        }
+        /// <summary>
+        /// Metodo costruttore del ViewModel.
+        /// </summary>
+        public MainViewModel() { RegisterCommands(); }
 
         private void RegisterCommands()
         {
             benvenutoViewModel = new BenvenutoViewModel();
             aboutViewModel = new AboutViewModel();
+            esercizioListViewModel = new EsercizioListViewModel();
             AboutCommand = new DelegateCommand(OnAbout, CanAbout);
             BenvenutoCommand = new DelegateCommand(OnBenvenuto, CanBenvenuto);
+            EsercizioListCommand = new DelegateCommand(OnEsercizioList, CanEsercizioList);
             CurrentViewModel = benvenutoViewModel;
             RegisterMessages();
         }
 
+        /// <summary>
+        /// Ricezione dei messaggi da parte degli altri ViewModels.
+        /// </summary>
         private void RegisterMessages()
         {
-            Messenger.Default.Register<BindableBase>(this, OnCurrentViewModelReceived);
+            Messenger.Default.Register<BindableBase>(this, OnViewModelReceived);
+        }
+
+        /// <summary>
+        /// Impostazione del ViewModel tramite la ricezione del messaggio.
+        /// </summary>
+        /// <param name="viewmodel">ViewModel da impostare.</param>
+        public void OnViewModelReceived(BindableBase viewmodel)
+        {
+            CurrentViewModel = viewmodel;
         }
 
         private void OnBenvenuto(object obj) { CurrentViewModel = benvenutoViewModel; }
         private bool CanBenvenuto(object arg) { return true; }
         private void OnAbout(object obj) { CurrentViewModel = aboutViewModel; }
         private bool CanAbout(object arg) { return true; }
-        public void OnCurrentViewModelReceived(BindableBase viewmodel) { CurrentViewModel = viewmodel; }
+        private void OnEsercizioList(object obj) { CurrentViewModel = esercizioListViewModel; }
+        private bool CanEsercizioList(object arg) { return true; }
     }
 }
