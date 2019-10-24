a414bf1313155c9e44e702999251bb3d0e5009fd
gabrialessi <gabrialessi23@gmail.com>
2019-10-24 14:26:15 +0200

    AboutViewModel con Messenger

--- Project/GestioneEsercizi/GestioneEsercizi/ViewModels/AboutViewModel.cs
+++ Project/GestioneEsercizi/GestioneEsercizi/ViewModels/AboutViewModel.cs
@@ -7,11 +7,37 @@ namespace GestioneEsercizi.ViewModels
 {
     public class AboutViewModel : BindableBase
     {
+        private BenvenutoViewModel benvenutoViewModel;
+
+        public IDelegateCommand BenvenutoCommand { get; set; }
+
+        private BindableBase selectedViewModel;
+        public BindableBase SelectedViewModel
+        {
+            get { return selectedViewModel; }
+            set { SetProperty(ref selectedViewModel, value); }
+        }
+
         /// <summary>
         /// Metodo costruttore del ViewModel.
         /// </summary>
         public AboutViewModel()
         {
+            RegisterCommands();
         }
+
+        private void RegisterCommands()
+        {
+            benvenutoViewModel = new BenvenutoViewModel();
+            BenvenutoCommand = new DelegateCommand(OnBenvenuto, CanBenvenuto);
+        }
+
+        private void EditCurrentViewModel(object obj)
+        {
+            Messenger.Default.Send<BindableBase>(selectedViewModel);
+        }
+
+        private void OnBenvenuto(object obj) { SelectedViewModel = benvenutoViewModel; }
+        private bool CanBenvenuto(object arg) { return true; }
     }
 }
