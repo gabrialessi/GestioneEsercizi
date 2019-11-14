36210ce2ea9f1171c7c9bf67da8cde91240474cc
gabrialessi <gabrialessi23@gmail.com>
2019-11-14 14:09:23 +0100

    EsercizioView drag&drop immagine

--- Project/GestioneEsercizi/GestioneEsercizi/Views/EsercizioView.xaml.cs
+++ Project/GestioneEsercizi/GestioneEsercizi/Views/EsercizioView.xaml.cs
@@ -1,17 +1,6 @@
-﻿using GestioneEsercizi.DA;
-using GestioneEsercizi.DA.Services;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GestioneEsercizi.Views
 {
@@ -24,30 +13,23 @@ namespace GestioneEsercizi.Views
         {
             InitializeComponent();
         }
-
         private void bSfoglia_Click(object sender, RoutedEventArgs e)
         {
-            // Create OpenFileDialog 
+            // Creazione File Explorer
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-
-            // Set filter for file extension and default file extension 
-            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
-
-            // Display OpenFileDialog by calling ShowDialog method 
-            Nullable<bool> result = dlg.ShowDialog();
-
-            // Get the selected file name and display in a TextBox 
-            if (result == true) lImmagine.Content = dlg.FileName;
+            // Impostazione estensioni files
+            dlg.Filter = "Image Files | *.jpg; *.jpeg; *.png; *.gif";
+            // Mostrare il file selezionato nella label
+            if (dlg.ShowDialog() == true) SetLabelContent(dlg.FileName);
         }
-
-        private void Grid_Drop(object sender, DragEventArgs e)
+        private void Image_Drop(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
-                // Note that you can have more than one file.
                 string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                lImmagine.Content = files[0].ToString();
+                SetLabelContent(files[0]);
             }
         }
+        private void SetLabelContent(string filename) => lImmagine.Content = Path.GetFileName(filename);
     }
 }
