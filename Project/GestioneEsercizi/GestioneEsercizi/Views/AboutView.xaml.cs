5a8f95f54893e6b3908cf3a0c252b3ca87a04a29
gabrialessi <gabrialessi23@gmail.com>
2019-11-15 14:26:32 +0100

    AboutView concluso

--- Project/GestioneEsercizi/GestioneEsercizi/Views/AboutView.xaml.cs
+++ Project/GestioneEsercizi/GestioneEsercizi/Views/AboutView.xaml.cs
@@ -1,16 +1,19 @@
-﻿using System;
+﻿using System.Diagnostics;
 using System.Windows.Controls;
 
 namespace GestioneEsercizi.Views
 {
-    /// <summary>
-    /// Interaction logic for AboutView.xaml
-    /// </summary>
     public partial class AboutView : UserControl
     {
-        public AboutView()
+        public AboutView() => InitializeComponent();
+        private void NavigateWebsite(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
         {
-            InitializeComponent();
+            try
+            {
+                Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+                e.Handled = true;
+            }
+            catch (System.ComponentModel.Win32Exception) { }
         }
     }
 }
