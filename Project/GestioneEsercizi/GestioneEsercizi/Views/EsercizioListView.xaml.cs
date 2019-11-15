a4c72e308bdb774a34fe9b748b6824968e609cc8
gabrialessi <gabrialessi23@gmail.com>
2019-11-15 15:31:42 +0100

    Esercizio e Prova ListView conclusi

--- Project/GestioneEsercizi/GestioneEsercizi/Views/EsercizioListView.xaml.cs
+++ Project/GestioneEsercizi/GestioneEsercizi/Views/EsercizioListView.xaml.cs
@@ -1,29 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace GestioneEsercizi.Views
 {
-    /// <summary>
-    /// Interaction logic for EsercizioListView.xaml
-    /// </summary>
     public partial class EsercizioListView : UserControl
     {
         public EsercizioListView()
         {
             InitializeComponent();
-            //dgEsercizi.Columns[1].Visibility = Visibility.Collapsed;
-            //dgEsercizi.Columns[3].Visibility = Visibility.Collapsed;
-            //dgEsercizi.Columns[4].Visibility = Visibility.Collapsed;
+            //eserciziDataGrid.Columns[0].Visibility = Visibility.Collapsed;
         }
     }
 }
--- Project/GestioneEsercizi/GestioneEsercizi/Views/ProvaListView.xaml.cs
+++ Project/GestioneEsercizi/GestioneEsercizi/Views/ProvaListView.xaml.cs
@@ -1,26 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Windows.Controls;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GestioneEsercizi.Views
 {
-    /// <summary>
-    /// Interaction logic for ProvaListView.xaml
-    /// </summary>
     public partial class ProvaListView : UserControl
     {
         public ProvaListView()
         {
             InitializeComponent();
+            //proveDataGrid.Columns[0].Visibility = Visibility.Collapsed;
         }
     }
 }
