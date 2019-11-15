eac9e3f33821befc0207b01164ba13336071c00a
gabrialessi <gabrialessi23@gmail.com>
2019-11-15 16:06:43 +0100

    ImpostazioniBaseView concluso

--- Project/GestioneEsercizi/GestioneEsercizi/Views/ImpostazioniBaseView.xaml.cs
+++ Project/GestioneEsercizi/GestioneEsercizi/Views/ImpostazioniBaseView.xaml.cs
@@ -1,27 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace GestioneEsercizi.Views
 {
-    /// <summary>
-    /// Interaction logic for ImpostazioniBaseView.xaml
-    /// </summary>
     public partial class ImpostazioniBaseView : UserControl
     {
         public ImpostazioniBaseView()
         {
             InitializeComponent();
-            //dgClassi.Columns[0].Visibility = Visibility.Collapsed;
         }
     }
 }
