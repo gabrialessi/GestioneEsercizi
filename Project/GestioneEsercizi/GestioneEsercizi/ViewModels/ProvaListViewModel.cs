d994e8d75a2ae34654446f90d2f4148a4d003540
gabrialessi <gabrialessi23@gmail.com>
2019-11-19 14:04:58 +0100

    ProvaViewModel concluso

--- Project/GestioneEsercizi/GestioneEsercizi/ViewModels/ProvaListViewModel.cs
+++ Project/GestioneEsercizi/GestioneEsercizi/ViewModels/ProvaListViewModel.cs
@@ -2,10 +2,7 @@
 using GestioneEsercizi.DA.Models;
 using GestioneEsercizi.DA.Services;
 using MVVM;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Text;
 
 namespace GestioneEsercizi.ViewModels
 {
@@ -14,32 +11,28 @@ namespace GestioneEsercizi.ViewModels
     /// </summary>
     public class ProvaListViewModel : BindableBase
     {
+        /// <summary>
+        /// Campo che rappresenta il ViewModel della schermata di benvenuto.
+        /// </summary>
         private BenvenutoViewModel benvenutoViewModel;
+        /// <summary>
+        /// Comando che porta alla schermata di benvenuto.
+        /// </summary>
         public IDelegateCommand BenvenutoCommand { get; set; }
         /// <summary>
         /// Lista di prove.
         /// </summary>
         public ObservableCollection<Prova> Prove { get; set; }
-
         /// <summary>
         /// Metodo costruttore del ViewModel.
         /// </summary>
         public ProvaListViewModel()
         {
-            RegisterCommands();
+            benvenutoViewModel = new BenvenutoViewModel();
+            BenvenutoCommand = new DelegateCommand(OnBenvenuto, CanBenvenuto);
             ProvaDbRepository repo = new ProvaDbRepository(new AppDbContext());
             Prove = new ObservableCollection<Prova>(repo.Get());
         }
-
-        /// <summary>
-        /// Impostazione dei ViewModels e Commands relativi.
-        /// </summary>
-        private void RegisterCommands()
-        {
-            benvenutoViewModel = new BenvenutoViewModel();
-            BenvenutoCommand = new DelegateCommand(OnBenvenuto, CanBenvenuto);
-        }
-
         private void OnBenvenuto(object obj) { Messenger.Default.Send<BindableBase>(benvenutoViewModel); }
         private bool CanBenvenuto(object arg) { return true; }
     }
