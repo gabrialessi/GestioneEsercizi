48555bb8d5638a23da33af1884676624dbbafeb1
gabrialessi <gabrialessi23@gmail.com>
2019-11-22 15:22:05 +0100

    Repository Anno creato

---
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Services/AnnoDbRepository.cs
@@ -0,0 +1,22 @@
+﻿using GestioneEsercizi.DA.Models;
+using System.Linq;
+
+namespace GestioneEsercizi.DA.Services
+{
+    /// <summary>
+    /// Deposito dati relativo al modello di dati della classe.
+    /// </summary>
+    public class AnnoDbRepository : DbDataRepository<AppDbContext, Anno>, IAnnoRepository
+    {
+        /// <summary>
+        /// Metodo costruttore, si definisce il contesto di dati.
+        /// </summary>
+        /// <param name="ctx">Contesto di dati del database.</param>
+        public AnnoDbRepository(AppDbContext ctx) : base(ctx) { }
+        /// <summary>
+        /// Ritorna tutte le entità ordinate per anno.
+        /// </summary>
+        /// <returns>Le entità ordinate per anno.</returns>
+        public override IQueryable<Anno> Get() => base.Get().OrderByDescending(s => s.Annata);
+    }
+}
---
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Services/IAnnoRepository.cs
@@ -0,0 +1,10 @@
+﻿using GestioneEsercizi.DA.Models;
+
+namespace GestioneEsercizi.DA.Services
+{
+    /// <summary>
+    /// Interfaccia che implementa i metodi relativi al database
+    /// sul modello di dati dell'anno.
+    /// </summary>
+    public interface IAnnoRepository : IDataRepository<Anno> { }
+}
