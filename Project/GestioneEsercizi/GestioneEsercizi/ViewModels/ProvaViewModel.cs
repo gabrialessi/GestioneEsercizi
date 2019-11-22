fb22f1b49105a5dc53d80b09c9c95abb26d2464c
gabrialessi <gabrialessi23@gmail.com>
2019-11-22 15:57:58 +0100

    ProvaViewModel creato

--- Project/GestioneEsercizi/GestioneEsercizi/ViewModels/ProvaViewModel.cs
+++ Project/GestioneEsercizi/GestioneEsercizi/ViewModels/ProvaViewModel.cs
@@ -12,10 +12,51 @@ namespace GestioneEsercizi.ViewModels
     public class ProvaViewModel : BindableBase
     {
         /// <summary>
+        /// Comando che porta alla schermata di benvenuto.
+        /// </summary>
+        public IDelegateCommand BenvenutoCommand { get; set; }
+        /// <summary>
+        /// Comando che genera la prova.
+        /// </summary>
+        public IDelegateCommand SalvaCommand { get; set; }
+        /// <summary>
+        /// Insieme dinamico dei dati degli anni.
+        /// </summary>
+        public ObservableCollection<Anno> Anni { get; set; }
+        /// <summary>
+        /// Insieme dinamico dei dati degli esercizi.
+        /// </summary>
+        public ObservableCollection<Esercizio> Esercizi { get; set; }
+        /// <summary>
+        /// Titolo della prova.
+        /// </summary>
+        public string Titolo { get; set; }
+        /// <summary>
+        /// Data della prova.
+        /// </summary>
+        public DateTime Data { get; set; }
+        /// <summary>
+        /// Anno della prova.
+        /// </summary>
+        public Anno Anno { get; set; }
+        /// <summary>
         /// Metodo costruttore del ViewModel.
         /// </summary>
         public ProvaViewModel()
         {
+            BenvenutoCommand = new DelegateCommand(OnBenvenuto, CanBenvenuto);
+            SalvaCommand = new DelegateCommand(OnSalva, CanSalva);
+            AnnoDbRepository repoAnno = new AnnoDbRepository(new AppDbContext());
+            EsercizioDbRepository repoEsercizio = new EsercizioDbRepository(new AppDbContext());
+            Anni = new ObservableCollection<Anno>(repoAnno.Get());
+            Esercizi = new ObservableCollection<Esercizio>(repoEsercizio.Get());
         }
+        private void OnBenvenuto(object obj) => Messenger.Default.Send<BindableBase>(new BenvenutoViewModel());
+        private bool CanBenvenuto(object arg) => true;
+        private void OnSalva(object obj)
+        {
+            OnBenvenuto(obj);
+        }
+        private bool CanSalva(object arg) => true;
     }
 }
