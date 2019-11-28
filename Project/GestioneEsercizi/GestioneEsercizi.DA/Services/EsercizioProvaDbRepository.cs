fff2fce2afa3d8c3bcebc08c4943a2e1ba9f498c
gabrialessi <gabrialessi23@gmail.com>
2019-11-28 15:15:58 +0100

    EsercizioProva Services

---
+++ Project/GestioneEsercizi/GestioneEsercizi.DA/Services/EsercizioProvaDbRepository.cs
@@ -0,0 +1,16 @@
+﻿using GestioneEsercizi.DA.Models;
+
+namespace GestioneEsercizi.DA.Services
+{
+    /// <summary>
+    /// Deposito dati relativo al modello di dati dell'EsercizioProva.
+    /// </summary>
+    public class EsercizioProvaDbRepository : DbDataRepository<AppDbContext, EsercizioProva>, IEsercizioProvaRepository
+    {
+        /// <summary>
+        /// Metodo costruttore, si definisce il contesto di dati.
+        /// </summary>
+        /// <param name="ctx">Contesto di dati del database.</param>
+        public EsercizioProvaDbRepository(AppDbContext ctx) : base(ctx) { }
+    }
+}
