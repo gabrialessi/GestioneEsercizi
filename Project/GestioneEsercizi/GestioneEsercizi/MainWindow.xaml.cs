d0a5d4914b84de3bd4f0aa0cdd135aa4009847d2
gabrialessi <gabrialessi23@gmail.com>
2019-12-06 15:03:13 +0100

    Algoritmo inserimento anno

--- Project/GestioneEsercizi/GestioneEsercizi/MainWindow.xaml.cs
+++ Project/GestioneEsercizi/GestioneEsercizi/MainWindow.xaml.cs
@@ -1,9 +1,29 @@
-﻿using System.Windows;
+﻿using GestioneEsercizi.DA;
+using GestioneEsercizi.DA.Models;
+using GestioneEsercizi.DA.Services;
+using System;
+using System.Linq;
+using System.Windows;
 
 namespace GestioneEsercizi
 {
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AnnoDbRepository repo = new AnnoDbRepository(new AppDbContext());
+            string anno = DateTime.Today.Year.ToString()
+                    + "/" + (DateTime.Today.Year + 1).ToString();
+            try
+            {
+                if (repo.Get().FirstOrDefault().Annata != anno) repo.Insert(new Anno(anno));
+            }
+            catch (NullReferenceException)
+            {
+                repo.Insert(new Anno(anno));
+            }
+        }
     }
 }
