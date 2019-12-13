d4dc4f0e7ea51c8da97f3602eb042f97151bbdf9
gabrialessi <gabrialessi23@gmail.com>
2019-12-13 14:18:35 +0100

    Correzioni ProvaView

--- Project/GestioneEsercizi/GestioneEsercizi/Views/ProvaView.xaml.cs
+++ Project/GestioneEsercizi/GestioneEsercizi/Views/ProvaView.xaml.cs
@@ -5,12 +5,6 @@ namespace GestioneEsercizi.Views
 {
     public partial class ProvaView : UserControl
     {
-        public ProvaView()
-        {
-            InitializeComponent();
-            dataDatePicker.DisplayDateStart = new DateTime(DateTime.Now.Year,
-                                                           DateTime.Now.Month,
-                                                           DateTime.Now.Day);
-        }
+        public ProvaView() => InitializeComponent();
     }
 }
