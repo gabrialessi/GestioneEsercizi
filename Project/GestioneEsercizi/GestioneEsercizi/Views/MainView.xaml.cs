3de57694c49d69c65f4b714cbaec65aeb8a4a605
gabrialessi <gabrialessi23@gmail.com>
2019-12-17 15:21:28 +0100

    doc - concluso MainView

--- Project/GestioneEsercizi/GestioneEsercizi/Views/MainView.xaml.cs
+++ Project/GestioneEsercizi/GestioneEsercizi/Views/MainView.xaml.cs
@@ -6,7 +6,10 @@ namespace GestioneEsercizi.Views
     public partial class MainView : UserControl
     {
         public MainView() => InitializeComponent();
-        private void miEsci_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Chiusura dell'applicazione.
+        /// </summary>
+        private void Exit(object sender, RoutedEventArgs e)
             => System.Windows.Application.Current.Shutdown();
     }
 }
